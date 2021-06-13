1f80d88e36c4b318941925771d55c2e79d940dfd
christian cancedda <christian.cancedda12@gmail.com>
2021-06-13 12:31:23 +0200

    d

--- lib/data/utils.py
+++ lib/data/utils.py
@@ -41,7 +41,15 @@ def recursive_visit(curr_path, samples, filename_pattern, replacement_string, co
             return counter
 
 
-
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
 
 
 class DriveDownloader(object):
@@ -84,13 +92,3 @@ class DriveDownloader(object):
 
 
 
-
-  def str2bool(v):
-    if isinstance(v, bool):
-        return v
-    if v.lower() in ('yes', 'true', 't', 'y', '1'):
-        return True
-    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
-        return False
-    else:
-        raise argparse.ArgumentTypeError('Boolean value expected.')
