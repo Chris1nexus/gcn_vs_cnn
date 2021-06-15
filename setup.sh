f48e29853f0df174ff915ba6cb19438271b3d4ea
christian cancedda <christian.cancedda12@gmail.com>
2021-06-15 19:33:00 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,3 +2,4 @@ pip install scikit-build
 pip install -r requirements.txt
 sudo apt-get install python3-dev graphviz libgraphviz-dev pkg-config
 pip install pygraphviz
+pip install opencv-contrib-python
